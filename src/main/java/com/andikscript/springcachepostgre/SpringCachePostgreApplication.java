299690dc9b69e9126035e21a6e4aa0b2c5d22948
andikscript <andikscript@gmail.com>
2022-08-23 06:23:55 +0700

    initial commit

---
+++ src/main/java/com/andikscript/springcachepostgre/SpringCachePostgreApplication.java
@@ -0,0 +1,13 @@
+package com.andikscript.springcachepostgre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCachePostgreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCachePostgreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andikscript/springcachepostgre/SpringCachePostgreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andikscript.springcachepostgre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCachePostgreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
