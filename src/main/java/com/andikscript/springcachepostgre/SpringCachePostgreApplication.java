f8a5667073c38586d6a821b000b14329bdf7424d
andikscript <andikscript@gmail.com>
2022-08-23 06:33:47 +0700

    add chaching

--- src/main/java/com/andikscript/springcachepostgre/SpringCachePostgreApplication.java
+++ src/main/java/com/andikscript/springcachepostgre/SpringCachePostgreApplication.java
@@ -2,8 +2,10 @@ package com.andikscript.springcachepostgre;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class SpringCachePostgreApplication {
 
 	public static void main(String[] args) {
